7c0a35789acb2a87bdbcd699c7c0a04599ff5e52
Chen Chen PhD SPIRIT <s190009@scsegc.cm.cluster>
2020-10-18 13:15:00 +0800

    debug

--- models/model_ComplEx.py
+++ models/model_ComplEx.py
@@ -16,21 +16,30 @@ class ComplEx(nn.Module):
         self.rel_re_embed = nn.Embedding(n_rel, self.depth)
         self.rel_im_embed = nn.Embedding(n_rel, self.depth)
 
+        self.dropout_ent_re = nn.Dropout(configs.dropout)
+        self.dropout_ent_im = nn.Dropout(configs.dropout)
+        self.dropout_rel_re = nn.Dropout(configs.dropout)
+        self.dropout_rel_im = nn.Dropout(configs.dropout)
+
         self.all_params = [self.ent_re_embed, self.ent_im_embed, self.rel_re_embed, self.rel_im_embed]
-        self.initialize()
 
     def initialize(self):
         for emb in self.all_params:
-            nn.init.xavier_normal_(emb)
+            nn.init.xavier_normal_(emb.weight)
 
         self.rel_re_embed.weight.data = F.normalize(self.rel_re_embed.weight.data, dim=1)
         self.rel_im_embed.weight.data = F.normalize(self.rel_im_embed.weight.data, dim=1)
 
     def get_score(self, heads, tails, rels, clamp=True):
-        score = torch.sum(self.rel_re_embed(rels) * self.ent_re_embed(heads) * self.ent_re_embed(tails), dim=-1) \
-            + torch.sum(self.rel_re_embed(rels) * self.ent_im_embed(heads) * self.ent_im_embed(tails), dim=-1) \
-            + torch.sum(self.rel_im_embed(rels) * self.ent_re_embed(heads) * self.ent_im_embed(tails), dim=-1) \
-            - torch.sum(self.rel_im_embed(rels) * self.ent_im_embed(heads) * self.ent_re_embed(tails), dim=-1)
+        heads_re = self.dropout_ent_re(self.ent_re_embed(heads))
+        heads_im = self.dropout_ent_im(self.ent_im_embed(heads))
+        tails_re = self.dropout_ent_re(self.ent_re_embed(tails))
+        tails_im = self.dropout_ent_im(self.ent_im_embed(tails))
+        rels_re = self.dropout_rel_re(self.rel_re_embed(rels))
+        rels_im = self.dropout_rel_im(self.rel_im_embed(rels))
+
+        score = torch.sum(rels_re * heads_re * tails_re, dim=-1) + torch.sum(rels_re * heads_im * tails_im, dim=-1) \
+            + torch.sum(rels_im * heads_re * tails_im, dim=-1) - torch.sum(rels_im * heads_im * tails_re, dim=-1)
         if clamp:
             score = torch.clamp(score, -20, 20)
         return score
